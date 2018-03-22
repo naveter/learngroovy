83543e919916ceee2158a69ad96e032005bbd06f
naveter <naveter@gmail.com>
2018-03-22 17:40:06 +0300

    UserTest added

--- src/groovy/scripts/parseXML.groovy
+++ src/groovy/scripts/parseXML.groovy
@@ -1,5 +1,7 @@
 package scripts
 
+//def result = '../../resourse/parseXML.xml'.toURL().text
+//def catalog = new XmlSlurper().parseText(result)
 def catalog = new XmlSlurper().parse("../../resourse/parseXML.xml")
-catalog.children().each {println it.toString() }
+catalog.children().each { println it.toString() }
 

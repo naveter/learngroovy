f173933429476368e9bd892557cc32dd551f3d0f
naveter <naveter@gmail.com>
2018-03-23 11:35:25 +0300

    dateTime added

---
+++ src/groovy/scripts/dateTime.groovy
@@ -0,0 +1,18 @@
+package scripts
+
+import groovy.time.TimeCategory
+
+def acceptedFormat = "yyyy-MM-dd"
+def today = new Date() + 1
+def tommorow = today.format(acceptedFormat)
+println( "Tommorow: ${tommorow}")
+
+use(TimeCategory) {
+    def oneYear = today + 1.year
+    println "One year plus: " + oneYear
+
+    def ninetyDays = today + 1.months
+    println "One month plus: " + ninetyDays
+}
+
+
